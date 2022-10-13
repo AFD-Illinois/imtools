188dfb6a4a56f3e689dde0a2ab2d96e39b92f989
Ben Prather <bprathr2@illinois.edu>
2022-10-13 16:18:47 -0600

    Optional psutil, add nthreads_calc to parallel

--- imtools/parallel.py
+++ imtools/parallel.py
@@ -35,10 +35,12 @@ __license__ = """
 import multiprocessing
 from tqdm.auto import tqdm
 
-# Hack for passing lambdas to a parallel function:
-# Initialize with a dangling function pointer and fill it at call time
-# Credit https://medium.com/@yasufumy/python-multiprocessing-c6d54107dd55
-_func = None
+try:
+    import psutil
+    using_psutil = True
+except ModuleNotFoundError as e:
+    print("Not using psutil: ", e)
+    using_psutil = False
 
 __doc__ = \
 """Tools for running embarrassingly parallel operations using multiple processes.
@@ -46,6 +48,11 @@ Basically makes multiprocessing more usable and adds conveniences like progress 
 You probably want map_parallel, which maps a function of your choice (defined or lambda)
 over a list or iterator, and returns the result.
 """
+
+# Hack for passing lambdas to a parallel function:
+# Initialize with a dangling function pointer and fill it at call time
+# Credit https://medium.com/@yasufumy/python-multiprocessing-c6d54107dd55
+_func = None
 
 def _worker_init(func):
     global _func
@@ -91,6 +98,23 @@ def iter_parallel(function, merge_function, input_list, output, nprocs=None, ini
     else:
         pool.close()
         pool.join()
+
+# Calculate ideal # threads
+# Lower pad values are safer
+def calc_nthreads(hdr, n_mkl=8, pad=0.25):
+    # Limit threads for 192^3+ problem due to memory
+    if using_psutil:
+        # Roughly compute memory and leave some generous padding for multiple copies and Python games
+        # (N1*N2*N3*8)*(NPRIM + 4*4 + 6) = size of "dump," (N1*N2*N3*8)*(2*4*4 + 6) = size of "geom"
+        # TODO get a better model for this!!
+        ncopies = hdr['n_prim'] + 4 * 4 + 6
+        nproc = int(pad * psutil.virtual_memory().total / (hdr['n1'] * hdr['n2'] * hdr['n3'] * 8 * ncopies))
+        if nproc < 1:
+            nproc = 1
+    else:
+        print("psutil not available: Using 4 processes as a safe default")
+
+    return nproc
 
 def set_mkl_threads(n_mkl):
     """Try to set the MKL numpy backend to use ``n_mkl`` threads."""
